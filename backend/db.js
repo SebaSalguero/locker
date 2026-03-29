f172de229c2c369d3c14235f3159532264d7d98c
Sebastian Salguero <sebac1984@hotmail.com>
2026-03-29 17:44:50 -0300

    fix sintaxis db

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,13 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "locker"
-});
+const db = mysql.createConnection(process.env.MYSQL_PUBLIC_URL);
 
 db.connect(err => {
   if (err) {
-    console.error("Error conectando a MySQL:", err);
+    console.error("❌ Error conectando a MySQL:", err);
     return;
   }
-  console.log("Base de datos conectada");
+  console.log("✅ Base de datos conectada");
 });
 
 module.exports = db;
