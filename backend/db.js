e0f3eece4efda79c1bbe4961f9c1c25c907d9914
Sebastian Salguero <sebac1984@hotmail.com>
2026-04-12 00:12:05 -0300

    Actualizar conexion a pool

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,11 @@
 const mysql = require("mysql2");
-console.log("MYSQL_PUBLIC_URL:", process.env.MYSQL_PUBLIC_URL);
-const db = mysql.createConnection(process.env.MYSQL_PUBLIC_URL);
 
-db.connect(err => {
-  if (err) {
-    console.error("❌ Error conectando a MySQL:", err);
-    return;
-  }
-  console.log("✅ Base de datos conectada");
+
+const db = mysql.createPool({
+  uri: process.env.MYSQL_PUBLIC_URL,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = db;
