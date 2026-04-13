c4c17948e3b03fe2b1b67bb3d6cb963a1dea32a4
Sebastian Salguero <sebac1984@hotmail.com>
2026-04-12 23:08:54 -0300

    conexion a cloudinary

--- backend/server.js
+++ backend/server.js
@@ -38,3 +38,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Servidor corriendo en puerto", PORT);
 });
+
+console.log("Cloudinary:", process.env.CLOUDINARY_CLOUD_NAME);
