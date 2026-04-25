5bc7f2c1cd5a45fa073044e50a99c92306503f37
Sebastian Salguero <sebac1984@hotmail.com>
2026-04-25 18:45:24 -0300

    Migramos a postgres

--- backend/db.js
+++ backend/db.js
@@ -1,22 +1,10 @@
 const { Pool } = require("pg");
 
-let db;
-
-if (process.env.DATABASE_URL) {
-  db = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
-} else {
-  db = new Pool({
-    host: "localhost",
-    user: "postgres",
-    password: "tu_password",
-    database: "postgres",
-    port: 5432
-  });
-}
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 module.exports = db;
